6c354add26679271bff2fa3db4b010bf7daba5ae
DavidLordCaos <davidlucas807@yahoo.com.br>
2021-07-26 11:53:43 -0300

    updated email body to send test in laravel

--- resources/views/mails/example.blade.php
+++ resources/views/mails/example.blade.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Example Mail Test</title>
+    <title>Bem Vindo!</title>
 </head>
 <body>
-    <h1>Hello from the other side!</h1>
-    <h3>if you're seeing this message!<br>
-        it means the test was successful!</h3>
+    <h1>Olá do outro lado!</h1>
+    <h3>Se você está vendo essa mensagem,<br>
+        o teste foi um sucesso!</h3>
+
+    <a href="http://0.0.0.0:8025/">Mailhog</a>
 </body>
 </html>
