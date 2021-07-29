30b9db58195c1ed13e89abd47742e5d5c28d7ae5
DavidLordCaos <davidlucas807@yahoo.com.br>
2021-07-29 09:21:58 -0300

    added swagger view

--- resources/views/sendMail.php
+++
@@ -1,9 +0,0 @@
-<h1>Email</h1>
-
-<?php
-$to = "test@mailhog.local";
-$subject = "Hey, I'm Your Father";
-$body = "Hello, MailHog!";
-$headers = "From: darth_vader@mailhog.com" . "\r\n";
-print_r(mail($to, $subject, $body, $headers));
-?>
